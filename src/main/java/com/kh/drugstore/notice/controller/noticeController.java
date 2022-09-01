c1e2e5cbbf3a18f348124e771c39f2e827a781f7
taeyeonkim-1207 <rlaxodus1207@naver.com>
2022-09-01 17:53:16 +0900

    notice

--- src/main/java/com/kh/drugstore/notice/controller/noticeController.java
+++ src/main/java/com/kh/drugstore/notice/controller/noticeController.java
@@ -1,14 +1,10 @@
 package com.kh.drugstore.notice.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.kh.drugstore.notice.model.dto.Notice;
 import com.kh.drugstore.notice.model.service.NoticeService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,9 +18,10 @@ public class noticeController {
 	NoticeService noticeService;
 	
 	@GetMapping("/noticeList.do")
-	public void noticeList() {
+	public ModelAndView noticeList(ModelAndView mav) {
 //		List<Notice> noticeList = noticeService.selectNoticeList();
-		
+		mav.setViewName("notice/noticeList");
+		return mav;
 	}
 	
 }
