7e991368c2f9629110b8ab0e1567008d034d8275
echojykim <honbaegsan@gmail.com>
2022-09-02 16:15:29 +0900

    진행중

--- src/main/java/com/kh/drugstore/product/model/dao/ProductDao.java
+++ src/main/java/com/kh/drugstore/product/model/dao/ProductDao.java
@@ -2,12 +2,21 @@ package com.kh.drugstore.product.model.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.session.RowBounds;
+
+import com.kh.drugstore.product.model.dto.Product;
 import com.kh.drugstore.product.model.dto.ProductAttachment;
 
-public class ProductDao {
+public interface ProductDao {
 
-	public static List<ProductAttachment> findAttachmentByProductCode(int pcode) {
-		
-	}
+
+	List<Product> findAllProduct(RowBounds rowBounds);
+
+	@Select("select count(*) from product")
+	int getTotalContent();
+
+	@Select("select * from product_attachment where pCode = #{pCode}")
+	List<ProductAttachment> findAttachmentByProductCode(int pCode);
 
 }
--- src/main/java/com/kh/drugstore/product/model/service/ProductService.java
+++ src/main/java/com/kh/drugstore/product/model/service/ProductService.java
@@ -1,5 +1,14 @@
 package com.kh.drugstore.product.model.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.kh.drugstore.product.model.dto.Product;
+
 public interface ProductService {
+
+	List<Product> findAllProduct(Map<String, Integer> param);
+
+	int getTotalContent();
 
 }
