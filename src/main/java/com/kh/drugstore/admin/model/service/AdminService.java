2cc6249d54fb7fc4a689b072974bb2c12cca76b4
Marmoriss <skawngml1106@naver.com>
2022-09-06 15:42:35 +0900

    카테고리 dto추가, 관리자페이지 상품 조회 작업중

--- src/main/java/com/kh/drugstore/admin/model/service/AdminService.java
+++ src/main/java/com/kh/drugstore/admin/model/service/AdminService.java
@@ -3,14 +3,18 @@ package com.kh.drugstore.admin.model.service;
 import java.util.List;
 
 import com.kh.drugstore.member.model.dto.Member;
+import com.kh.drugstore.product.model.dto.Category;
 import com.kh.drugstore.product.model.dto.Product;
 
 public interface AdminService {
 // 주희코드 시작
 	List<Product> selectProductList();
+	
+	List<Category> selectCategoryList(int categoryId);
 // 주희코드 끝
 
 // 태연코드 시작
 	List<Member> memberList();
 // 태연코드 끝
+
 }
